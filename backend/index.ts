26cd12c986a38f3dcd00aeeb49803f1796c2f7d8
kendrickeong <94017998+kendrickeong@users.noreply.github.com>
2024-12-21 16:09:40 +0800

    Add Basic JWT Implementation

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,51 @@
 import express from "express";
 import dotenv from "dotenv";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const JWT_SECRET = "your_secret_key";
+
+const users = [
+  {
+    id: 1,
+    username: "testuser",
+    password: "System123!", // Pre-hash the password for security
+  },
+];
+
 app.use(express.json());
+
+app.get("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if user exists
+  const user = users.find((u) => u.username === username);
+
+  console.log(user);
+
+  if (!user) {
+    res.status(401).json({ message: "Invalid username or password" });
+    return;
+  }
+
+  // Check if the password matches
+  if (password !== user.password) {
+    res.status(401).json({ message: "Invalid username or password" });
+    return;
+  }
+
+  const token = jwt.sign({ id: user.id, username: user.username }, JWT_SECRET, {
+    expiresIn: "1h",
+  });
+
+  // If everything is valid, return a success response
+  res.status(200).json({ message: "Login successful", token });
+});
 
 app.get("/api", (req, res) => {
   res.send("Hello from the backend!");
